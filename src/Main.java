614ffaa730a9ad3086e19f8404174e1fb5f84e38
Mateusz Duchalski <mduchalski1@gmail.com>
2019-05-24 13:05:48 +0200

    generic 3D experiments - rotating box

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,77 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.*;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-
+import javafx.scene.input.*;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
+import javafx.scene.transform.*;
+import javafx.stage.*;
 
 public class Main extends Application {
+    Box testBox;
 
-    final Group root = new Group();
+    public Group createContent() throws Exception {
+        // Box
+        testBox = new Box(5,5,5);
+        testBox.setMaterial(new PhongMaterial(Color.RED));
+        testBox.setDrawMode(DrawMode.FILL);
+
+        // Create and position camera
+        Camera camera = new PerspectiveCamera(true);
+        camera.getTransforms().addAll (
+                new Rotate(-20, Rotate.Y_AXIS),
+                new Rotate(-20, Rotate.X_AXIS),
+                new Translate(0, 0, -15));
+
+        // Lights
+        PointLight pLight = new PointLight(Color.WHITE);
+        pLight.setTranslateX(50);
+        pLight.setTranslateY(-300);
+        pLight.setTranslateZ(-400);
+        AmbientLight aLight = new AmbientLight(Color.color(0.3, 0.3, 0.3));
+
+        // Build the Scene Graph
+        Group root = new Group();
+        root.getChildren().addAll(camera, testBox, pLight, aLight);
+
+        // Use a SubScene
+        SubScene subScene = new SubScene(root, 500,500);
+        subScene.setFill(Color.ALICEBLUE);
+        subScene.setCamera(camera);
+        Group group = new Group();
+        group.getChildren().add(subScene);
+        return group;
+    }
 
     @Override
-    public void start(Stage primaryStage) {
-
-        Scene scene = new Scene(root, 1024, 768, true);
-        scene.setFill(Color.GREY);
-
-        primaryStage.setTitle("SCARA Robotic Arm");
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setResizable(false);
+        Scene scene = new Scene(createContent());
+        handleKeyboard(scene);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public void handleKeyboard(Scene scene) {
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case LEFT:
+                        testBox.getTransforms().add(new Rotate(-1, Rotate.Y_AXIS));
+                        break;
+                    case RIGHT:
+                        testBox.getTransforms().add(new Rotate(1, Rotate.Y_AXIS));
+                        break;
+                    case UP:
+                        testBox.getTransforms().add(new Rotate(1, Rotate.X_AXIS));
+                        break;
+                    case DOWN:
+                        testBox.getTransforms().add(new Rotate(-1, Rotate.X_AXIS));
+                        break;
+                }
+            }
+        });
 
     }
 
